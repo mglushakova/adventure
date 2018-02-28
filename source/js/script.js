43adc7a066198cf092c43f3fdec75f2fde80f7a5
mglushakova <glushakova7@mail.ru>
2018-02-28 14:41:35 +0500

    Исправляет поведение слайдера при переключении крайних слайдов

--- source/js/script.js
+++ source/js/script.js
@@ -7,23 +7,27 @@ for ( let i = 0; i < sliderButton.length; i++ ) {
     let slides = event.target.parentElement.nextElementSibling.querySelectorAll(".slider__item");
 
     if ( event.target.classList.contains("slider__prev") ) {
-      for ( let k = 0; k < slides.length; k++ ) {
+      for ( let k = 0; k <= slides.length - 1; k++ ) {
         if ( slides[k].classList.contains("slider__item--active") && k !== 0 ) {
           slides[k].classList.remove("slider__item--active");
           slides[k - 1].classList.add("slider__item--active");
+          break;
         } else if ( slides[k].classList.contains("slider__item--active") && k == 0 ) {
           slides[k].classList.remove("slider__item--active");
           slides[slides.length - 1].classList.add("slider__item--active");
+          break;
         }
       }
     } else if ( event.target.classList.contains("slider__next") ) {
       for ( let k = slides.length - 1; k >= 0; k-- ) {
-        if ( slides[k].classList.contains("slider__item--active") && k !== (slides.length - 1) ) {
+        if ( slides[k].classList.contains("slider__item--active") && k !== slides.length - 1 ) {
           slides[k].classList.remove("slider__item--active");
           slides[k + 1].classList.add("slider__item--active");
-        } else if ( slides[k].classList.contains("slider__item--active") && k == (slides.length - 1) ) {
+          break;
+        } else if ( slides[k].classList.contains("slider__item--active") && k == slides.length - 1 ) {
           slides[k].classList.remove("slider__item--active");
           slides[0].classList.add("slider__item--active");
+          break;
         }
       }
     }
